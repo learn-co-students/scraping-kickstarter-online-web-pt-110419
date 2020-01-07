02f720e51d292573a6fd05dbc750d7fd58abd721
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2020-01-07 16:01:09 +0000

    Done.

--- spec/kickstarter_scraper_spec.rb
+++ spec/kickstarter_scraper_spec.rb
@@ -30,3 +30,18 @@ def create_project_hash
   projects
 end
 end
+
+:projects => {
+  "My Great Project"  => {
+    :image_link => "Image Link",
+    :description => "Description",
+    :location => "Location",
+    :percent_funded => "Percent Funded"
+  },
+  "Another Great Project" => {
+    :image_link => "Image Link",
+    :description => "Description",
+    :location => "Location",
+    :percent_funded => "Percent Funded"
+  }
+}
